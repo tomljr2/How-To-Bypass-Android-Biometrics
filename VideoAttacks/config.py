00cc39ffc22873399672d97a132bc763c3a45e8a
Justin Tomlinson <justintomlinsonn@gmail.com>
2019-11-18 23:08:10 -0500

    Made decent progress with video capturing
    Justin Tomlinson
    Nov 18, 2019
    
    I still don't think this attack will be very effective, but regardless, I
    have made a good bit of progress. I need to modify how I record videos so
    that it doesn't start a new window every time it records, but for now, I'm
    just glad I have something working.
    
    I've also tested picture attacks some, and they are going to be far more
    effective, I think. However, I have yet to do an in-depth analysis on that.
    I'm waiting on some printer ink to come in the mail.

---
+++ VideoAttacks/config.py
@@ -0,0 +1,8 @@
+# Config settings that need to be changed for your computer.
+
+# To set, you will need to use xrandr to find the monitor output name,
+# (mine is DP-4), your resolution, and all of the available refresh
+# rates that your monitor supports.
+monitor="DP-4"
+resolution="2560x1440"
+rates=["60","100","120","144"]	# List of all supported refresh rates
