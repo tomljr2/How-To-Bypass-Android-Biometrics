c34b8e14c2fff9530bb74420fc1769f92a3bf7fb
Justin Tomlinson <justintomlinsonn@gmail.com>
2019-11-18 23:12:05 -0500

    Forgot to remove old file

--- VideoAttacks/videoattacks.py
+++
@@ -1,9 +0,0 @@
-from config import *
-import os
-import time
-
-for rate in rates:
-   os.system("xrandr --output " + monitor + " --mode " + resolution +	\
-             " -r " + rate)
-   os.system("xrandr")
-   time.sleep(5)
