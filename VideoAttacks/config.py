0ccb494436e6c8a56039603e59158c02e609ab02
Justin Tomlinson <justintomlinsonn@gmail.com>
2019-11-23 18:00:22 -0500

    small fix to cfps

--- VideoAttacks/config.py
+++ VideoAttacks/config.py
@@ -3,6 +3,9 @@
 cfps1=50
 cfps2=105
 cfps3=75
+cfps4=40
+cfps5=35
 
 # Do not change this
-cfpsList = {(1920,1080,30):cfps1,(1280,720,60):cfps2,(640,480,60):cfps3}
+cfpsList = {(1920,1080,30):cfps1,(1280,720,60):cfps2,(640,480,60):cfps3,\
+            (1280,720,30):cfps4,(640,480,30):cfps5}
